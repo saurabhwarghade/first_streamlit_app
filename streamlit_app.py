32ae7b49c4f22ab4ec92589872a1694e2e405d41
saurabhwarghade <43203821+saurabhwarghade@users.noreply.github.com>
2023-08-17 15:41:36 +0530

    Update streamlit_app.py
    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
-import streamlit
+import pandas
 
-streamlit.title('🥣 first test')
-streamlit.header('header')
-streamlit.text('header')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
