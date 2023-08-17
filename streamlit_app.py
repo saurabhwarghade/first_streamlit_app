8732c8362631455781aa90d3e3452fa984958028
saurabhwarghade <43203821+saurabhwarghade@users.noreply.github.com>
2023-08-17 15:35:57 +0530

    Update streamlit_app.py
    test2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@ import streamlit
 streamlit.title('first test')
 streamlit.header('header')
 streamlit.text('header')
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
