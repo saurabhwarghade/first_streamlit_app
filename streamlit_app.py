d825fc5c20a757f22541ca997d6bb0a396375394
saurabhwarghade <43203821+saurabhwarghade@users.noreply.github.com>
2023-08-17 15:39:30 +0530

    Update streamlit_app.py
    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,4 @@ import streamlit
 streamlit.title('🥣 first test')
 streamlit.header('header')
 streamlit.text('header')
-
-
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
