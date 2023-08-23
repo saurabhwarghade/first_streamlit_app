c3dd59e557781f3a70824b39ffbe2903a24ee206
saurabhwarghade <43203821+saurabhwarghade@users.noreply.github.com>
2023-08-23 20:36:32 +0530

    Update streamlit_app.py
    w

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
 import requests
+
+import snowflake.connector
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
