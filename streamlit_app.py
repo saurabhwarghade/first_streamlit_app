da9e0ac1c855869520ed8dd72cefe642c74ea07d
saurabhwarghade <43203821+saurabhwarghade@users.noreply.github.com>
2023-08-24 16:36:34 +0530

    Update streamlit_app.py
    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import requests
 
 
 
+
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
@@ -26,3 +28,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
