c0f92353fd4b868d352b38cf23d9e0bfc77b695a
saurabhwarghade <43203821+saurabhwarghade@users.noreply.github.com>
2023-08-24 20:22:27 +0530

    Update streamlit_app.py
    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response)
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
